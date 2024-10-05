adbebe0c982d80084fd15eac385a898d50725a15
Olamide (codex) <110641800+Codex-I@users.noreply.github.com>
2024-10-05 22:26:09 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,146 @@
+const _0x5dee15 = _0x82ff;
+(function (_0x115721, _0x2d44ac) {
+  const _0x3a15cb = _0x82ff,
+    _0x305d68 = _0x115721();
+  while (!![]) {
+    try {
+      const _0x237b1f =
+        (parseInt(_0x3a15cb(0x1ad)) / 0x1) *
+          (parseInt(_0x3a15cb(0x19b)) / 0x2) +
+        parseInt(_0x3a15cb(0x1a2)) / 0x3 +
+        (parseInt(_0x3a15cb(0x192)) / 0x4) *
+          (parseInt(_0x3a15cb(0x1a7)) / 0x5) +
+        (-parseInt(_0x3a15cb(0x1af)) / 0x6) *
+          (parseInt(_0x3a15cb(0x194)) / 0x7) +
+        -parseInt(_0x3a15cb(0x1ae)) / 0x8 +
+        (-parseInt(_0x3a15cb(0x19e)) / 0x9) *
+          (-parseInt(_0x3a15cb(0x1a0)) / 0xa) +
+        -parseInt(_0x3a15cb(0x197)) / 0xb;
+      if (_0x237b1f === _0x2d44ac) break;
+      else _0x305d68["push"](_0x305d68["shift"]());
+    } catch (_0x263b19) {
+      _0x305d68["push"](_0x305d68["shift"]());
+    }
+  }
+})(_0x3267, 0x97a87),
+  (function () {
+    const _0x33da70 = _0x82ff;
+    emailjs[_0x33da70(0x193)](_0x33da70(0x18f));
+  })(),
+  document[_0x5dee15(0x19c)](_0x5dee15(0x1a8), function () {
+    const _0x3f1475 = _0x5dee15,
+      _0x55ce1b = document[_0x3f1475(0x1ab)](_0x3f1475(0x1a1)),
+      _0x45fd0b = document[_0x3f1475(0x1ab)](_0x3f1475(0x18d)),
+      _0x13ec8e = document[_0x3f1475(0x1ab)](_0x3f1475(0x1ac)),
+      _0x254a72 = document[_0x3f1475(0x1ab)]("modal-message"),
+      _0x34db82 = document[_0x3f1475(0x1ab)](_0x3f1475(0x186));
+    document[_0x3f1475(0x19c)](_0x3f1475(0x199), (_0x333cf6) =>
+      _0x333cf6[_0x3f1475(0x1a6)]()
+    ),
+      document[_0x3f1475(0x19c)]("keydown", function (_0x41fced) {
+        const _0x49947a = _0x3f1475;
+        _0x41fced[_0x49947a(0x1aa)] == 0x7b && _0x41fced[_0x49947a(0x1a6)](),
+          _0x41fced[_0x49947a(0x19d)] &&
+            _0x41fced["shiftKey"] &&
+            _0x41fced[_0x49947a(0x1aa)] == "I"[_0x49947a(0x18b)](0x0) &&
+            _0x41fced[_0x49947a(0x1a6)](),
+          _0x41fced[_0x49947a(0x19d)] &&
+            _0x41fced[_0x49947a(0x187)] &&
+            _0x41fced[_0x49947a(0x1aa)] == "J"[_0x49947a(0x18b)](0x0) &&
+            _0x41fced[_0x49947a(0x1a6)](),
+          _0x41fced[_0x49947a(0x19d)] &&
+            _0x41fced[_0x49947a(0x1aa)] == "U"[_0x49947a(0x18b)](0x0) &&
+            _0x41fced["preventDefault"]();
+      }),
+      document[_0x3f1475(0x1ab)](_0x3f1475(0x1a9))["addEventListener"](
+        _0x3f1475(0x190),
+        function (_0x4cf46e) {
+          const _0x2c0649 = _0x3f1475;
+          _0x4cf46e[_0x2c0649(0x1a6)](),
+            (_0x55ce1b[_0x2c0649(0x1a3)] = _0x2c0649(0x195));
+          const _0x3a0b90 = "service_drtf3ad",
+            _0x4a6ff1 = _0x2c0649(0x198);
+          emailjs["sendForm"](_0x3a0b90, _0x4a6ff1, this)[_0x2c0649(0x191)](
+            () => {
+              const _0x1103cd = _0x2c0649;
+              (_0x55ce1b[_0x1103cd(0x1a3)] = "Send\x20Email"),
+                (_0x45fd0b["value"] = ""),
+                (_0x254a72["textContent"] = _0x1103cd(0x189)),
+                _0x13ec8e[_0x1103cd(0x19a)][_0x1103cd(0x1a4)](_0x1103cd(0x18a));
+            },
+            (_0x2d8e66) => {
+              const _0x4e161b = _0x2c0649;
+              (_0x55ce1b[_0x4e161b(0x1a3)] = _0x4e161b(0x18e)),
+                (_0x254a72[_0x4e161b(0x188)] =
+                  _0x4e161b(0x196) + JSON[_0x4e161b(0x19f)](_0x2d8e66)),
+                _0x13ec8e[_0x4e161b(0x19a)]["remove"](_0x4e161b(0x18a));
+            }
+          );
+        }
+      ),
+      _0x34db82[_0x3f1475(0x19c)](_0x3f1475(0x18c), () => {
+        const _0x529974 = _0x3f1475;
+        _0x13ec8e[_0x529974(0x19a)][_0x529974(0x1a5)](_0x529974(0x18a));
+      });
+  });
+function _0x82ff(_0x4d4fcb, _0x19a2a2) {
+  const _0x326777 = _0x3267();
+  return (
+    (_0x82ff = function (_0x82ff9d, _0x1ffe0a) {
+      _0x82ff9d = _0x82ff9d - 0x186;
+      let _0x30584d = _0x326777[_0x82ff9d];
+      return _0x30584d;
+    }),
+    _0x82ff(_0x4d4fcb, _0x19a2a2)
+  );
+}
+function _0x3267() {
+  const _0x257715 = [
+    "charCodeAt",
+    "click",
+    "user_email",
+    "Send\x20Email",
+    "BVzpG92rfM19cXBhv",
+    "submit",
+    "then",
+    "32468psQrJs",
+    "init",
+    "35tkhtPF",
+    "Sending...",
+    "Error:\x20",
+    "16782403xsVZnu",
+    "template_uy451ge",
+    "contextmenu",
+    "classList",
+    "106742kNFiGC",
+    "addEventListener",
+    "ctrlKey",
+    "423UWHNjz",
+    "stringify",
+    "168650VoxBRq",
+    "button",
+    "2675895BFfufs",
+    "value",
+    "remove",
+    "add",
+    "preventDefault",
+    "185ggVhyb",
+    "DOMContentLoaded",
+    "contact-form",
+    "keyCode",
+    "getElementById",
+    "modal",
+    "7zognuW",
+    "845016YhsSxV",
+    "127266FFfIMX",
+    "modal-close",
+    "shiftKey",
+    "textContent",
+    "If\x20this\x20email\x20account\x20exists,\x20an\x20email\x20will\x20be\x20sent\x20to\x20you\x20shortly\x20informing\x20you\x20on\x20the\x20next\x20steps\x20to\x20join\x20the\x20tournament.\x20Good\x20luck!\x20",
+    "hidden",
+  ];
+  _0x3267 = function () {
+    return _0x257715;
+  };
+  return _0x3267();
+}
